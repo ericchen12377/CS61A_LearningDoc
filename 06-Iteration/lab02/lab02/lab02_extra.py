cf31a18cd0f757d271a50059992cf56f3e4bb381
Suiyao Chen <suiyaochen@mail.usf.edu>
2020-04-30 15:36:30 -0400

    06-Iteration

---
+++ 06-Iteration/lab02/lab02/lab02_extra.py
@@ -0,0 +1,98 @@
+""" Optional problems for lab02 """
+
+from lab02 import *
+
+# Higher Order Functions
+
+def compose1(f, g):
+    """Return the composition function which given x, computes f(g(x)).
+
+    >>> add_one = lambda x: x + 1        # adds one to x
+    >>> square = lambda x: x**2
+    >>> a1 = compose1(square, add_one)   # (x + 1)^2
+    >>> a1(4)
+    25
+    >>> mul_three = lambda x: x * 3      # multiplies 3 to x
+    >>> a2 = compose1(mul_three, a1)    # ((x + 1)^2) * 3
+    >>> a2(4)
+    75
+    >>> a2(5)
+    108
+    """
+    return lambda x: f(g(x))
+
+def composite_identity(f, g):
+    """
+    Return a function with one parameter x that returns True if f(g(x)) is
+    equal to g(f(x)). You can assume the result of g(x) is a valid input for f
+    and vice versa.
+
+    >>> add_one = lambda x: x + 1        # adds one to x
+    >>> square = lambda x: x**2
+    >>> b1 = composite_identity(square, add_one)
+    >>> b1(0)                            # (0 + 1)^2 == 0^2 + 1
+    True
+    >>> b1(4)                            # (4 + 1)^2 != 4^2 + 1
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    return lambda x: f(g(x)) == g(f(x))
+
+def cycle(f1, f2, f3):
+    """Returns a function that is itself a higher-order function.
+
+    >>> def add1(x):
+    ...     return x + 1
+    >>> def times2(x):
+    ...     return x * 2
+    >>> def add3(x):
+    ...     return x + 3
+    >>> my_cycle = cycle(add1, times2, add3)
+    >>> identity = my_cycle(0)
+    >>> identity(5)
+    5
+    >>> add_one_then_double = my_cycle(2)
+    >>> add_one_then_double(1)
+    4
+    >>> do_all_functions = my_cycle(3)
+    >>> do_all_functions(2)
+    9
+    >>> do_more_than_a_cycle = my_cycle(4)
+    >>> do_more_than_a_cycle(2)
+    10
+    >>> do_two_cycles = my_cycle(6)
+    >>> do_two_cycles(1)
+    19
+    """
+    # def ret_fn(n):
+    #     def ret(x):
+    #         i = 0
+    #         while i < n:
+    #             if i % 3 == 0:
+    #                 x = f1(x)
+    #             elif i % 3 == 1:
+    #                 x = f2(x)
+    #             else:
+    #                 x = f3(x)
+    #             i += 1
+    #         return x
+    #     return ret
+    # return ret_fn
+    "*** YOUR CODE HERE ***"
+    def my_cycle(n):
+        def f_cycle(x):
+            i = 0
+            while i <= n:
+                if i == 0:
+                    x = x
+                elif i % 3 == 1:
+                    x = f1(x)
+                elif i % 3 == 2:
+                    x = f2(x)
+                elif i % 3 == 0 and i > 0:
+                    x = f3(x)
+                i += 1
+            return x
+        return f_cycle 
+    return my_cycle
+
